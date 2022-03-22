df58fb630a815f90057008bed0a0fe3f5fd5fb44
Zong-Rong Huang <zongrong.h886@gmail.com>
2022-03-22 19:21:05 +0800

    feat: make CardA a styled component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
-import CardA from "./components/CardA";
-import CardB from "./components/CardB";
+// import CardA from "./components/Cards/CardA";
+// import CardB from "./components/CardB";
+import CardA from "./components/Cards/CardA";
 
 function App() {
   return (
     <div className="App">
       <CardA />
+
       {/* <CardB /> */}
     </div>
   );
